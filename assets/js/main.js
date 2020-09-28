8926b58dc421f0e2a2f9597f89bbdf183c51a893
Riazul Islam <ri64627@gmail.com>
2020-09-28 13:15:24 +0600

    added sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,63 @@
-(function($) {
+// Set the date we're counting down to
+var countDownDate = new Date("November 5, 2020 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "0";
+    document.getElementById("hours").innerHTML = "0";
+    document.getElementById("minutes").innerHTML = "0";
+    document.getElementById("seconds").innerHTML = "0";
+  }
+}, 1000);
+
+(function ($) {
   "use strict";
 
-  jQuery(document).ready(function($) {
-    
+  jQuery(document).ready(function ($) {
+    // Smooth Scroll
+    var scroll = new SmoothScroll('a[href*="#"]', {
+      offset: 135,
+    });
+    //navbar Collapse
+    $(".navbar-collapse a").click(function () {
+      $(".navbar-collapse").collapse("hide");
+    });
+    $(".modal").each(function () {
+      var src = $(this).find("iframe").attr("src");
+
+      $(this).on("click", function () {
+        $(this).find("iframe").attr("src", "");
+        $(this).find("iframe").attr("src", src);
+      });
+    });
+  });
+  $(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 800) {
+      $(".scroll-to-top").addClass("active");
+    } else {
+      $(".scroll-to-top").removeClass("active");
+    }
   });
 })(jQuery);
